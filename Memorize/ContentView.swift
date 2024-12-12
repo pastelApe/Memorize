c00cf093dca8edcfd775178214dcbce99d7d33e7
Xavier Watson <xavier.watson@me.com>
2024-12-11 21:09:02 -0500

    Initial Commit Card Views isFaceUp

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -7,17 +7,64 @@
 
 import SwiftUI
 
+// Pretty much everything is a struct in SwiftUI
+// Not a class, not OOP
+
+// ContentView behaves like a View. Functions like, Functional Programmming.
+// Behavior, how this struct behaves like.
 struct ContentView: View {
+    // Computed Property. Everytime someone asks for body, it is run (read only). Not stored.
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack {
+            CardView(isFaceUp: true)
+            CardView()
+            CardView()
+            CardView()
         }
+        .foregroundColor(.orange)
         .padding()
     }
 }
+
+struct CardView: View {
+    var isFaceUp: Bool = false
+    
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                CardFrontView()
+            } else {
+                CardBackView()
+            }
+        }
+    }
+}
+
+
+struct CardFrontView: View {
+    var body: some View {
+        RoundedRectangle(cornerRadius: 12)
+            .foregroundColor(.white)
+        RoundedRectangle(cornerRadius: 12)
+            .strokeBorder(lineWidth: 2)
+        Text("👻").font(.largeTitle)
+    }
+}
+
+struct CardBackView: View {
+    var body: some View {
+        RoundedRectangle(cornerRadius: 12)
+    }
+}
+
+
+
+
+
+
+
+
 
 #Preview {
     ContentView()
