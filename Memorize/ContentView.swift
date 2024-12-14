f668754cd7a8bd8f91903eaed94e4351ac62313a
Xavier Watson <xavier.watson@me.com>
2024-12-13 22:56:47 -0500

    Used opacity to show front or back of card. Added scrolling. Used Group and LazyVStack

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -17,59 +17,82 @@ let card = RoundedRectangle(cornerRadius: 12)
 
 struct ContentView: View {
     // Computed Property. Everytime someone asks for body, it is run (read only). Not stored.
-    let emojis = ["👻", "🎃", "🧛🏻‍♂️", "🧙‍♀️"]
+    let emojis = ["👻", "🎃", "🧛🏻‍♂️", "🧙‍♀️", "😈", "🧙‍♂️", "🧟‍♀️", "☠️"]
+    
+    @State var cardCount = 4
     
     var body: some View {
-        HStack {
-            ForEach(emojis.indices, id: \.self) { emoji in
-                CardView(emoji: emojis[emoji])
+        VStack {
+            ScrollView{
+                cards
+            }
+            Spacer()
+            cardCountAdjusters
+        }
+        .padding()
+    }
+    
+    
+    // by is used by the caller, offset is for implementation
+    func cardCountAdjuster(by offset: Int, symbol: String) -> some View {
+        Button(action: {
+            cardCount += offset
+        }, label: {
+            Image(systemName: symbol)
+        })
+        .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
+    }
+        
+    var cardRemover: some View {
+        return cardCountAdjuster(by: -1, symbol: "rectangle.stack.badge.minus.fill")
+    }
+    
+    var cardAdder: some View {
+        return cardCountAdjuster(by: 1, symbol: "rectangle.stack.badge.plus.fill")
+    }
+    
+    // Not a viewbuilder. Just a function.  HStack is one line of code.
+    // This is implicit return.
+    var cards: some View {
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]) {
+            ForEach(0..<cardCount, id: \.self) { index in
+                CardView(emoji: emojis[index])
+                    .aspectRatio(2/3, contentMode: .fit)
             }
         }
         .foregroundColor(.orange)
-        .padding()
+    }
+    
+    var cardCountAdjusters: some View {
+        HStack {
+            cardRemover
+            Spacer()
+            cardAdder
+        }
+        .imageScale(.large)
+        .font(.largeTitle)
     }
 }
 
 struct CardView: View {
-    @State var isFaceUp = true //@State is a pointer, temporary state
-    
     let emoji: String
+    @State var isFaceUp = true //@State is a pointer, temporary state
     
     var body: some View {
         ZStack {
-            if isFaceUp {
-                CardFrontView(thisEmoji: emoji)
-            } else {
-                CardBackView()
+            Group {
+                card.fill(.white)
+                card.strokeBorder(lineWidth: 2)
+                Text(emoji).font(.largeTitle)
             }
-        }.onTapGesture {
+            .opacity(isFaceUp ? 1 : 0)
+            card.fill().opacity(isFaceUp ? 0 : 1)
+        }
+        .onTapGesture {
             isFaceUp.toggle()
         }
     }
 }
-
-
-struct CardFrontView: View {
-    let thisEmoji: String
-    
-    var body: some View {
-        card.foregroundColor(.white)
-        card.strokeBorder(lineWidth: 2)
-        Text(thisEmoji).font(.largeTitle)
-    }
-}
-
-struct CardBackView: View {
-    var body: some View {
-        card.fill()
-    }
-}
-
-
-
-
-
-
 
 
 
